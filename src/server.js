962838c135947304b5647437b4000dd614547017
GPMonkey <63848930+jhwq123@users.noreply.github.com>
2022-11-11 16:43:07 +0900

    NoonCloneUpdate2
    NoonCloneUpdate2

--- src/server.js
+++ src/server.js
@@ -7,21 +7,32 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.render("/"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+const sockets = [];
+
 wss.on("connection", (socket) => {
+  sockets.push(socket);
+  socket["nickname"] = "None";
   console.log("Connected to Browser!");
   socket.on("close", () => console.log("Disconnected from Server X"));
-  socket.on("message", (message) => {
-    console.log(message);
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}: ${message.payload}`)
+        );
+      case "nickname":
+        socket["nickname"] = message.payload;
+    }
   });
-  socket.send("hello!!!");
 });
 
 server.listen(3000, handleListen);
